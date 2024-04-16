8158c9544fc14df415be4c3a3f7139e6dddb3cb7
bharathjangam <bharathjangam@gmail.com>
2024-04-16 22:06:57 +1000

    Update LongestCommonPrefix

--- leetcode/easy/LongestCommonPrefix.java
+++ leetcode/easy/LongestCommonPrefix.java
@@ -1,5 +1,4 @@
 package leetcode.easy;
-
 /*
 Write a function to find the longest common prefix string amongst an array of strings.
 
@@ -27,25 +26,31 @@ strs[i] consists of only lowercase English letters.
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     private static void test1() {
         String[] strs = {"flower", "flow", "flight"};
         longestCommonPrefix(strs);
     }
+    private static void test2() {
+        String[] strs = {"c","acc","ccc"};
+        String result =  longestCommonPrefix(strs);
+        assert !result.equals("");
+    }
 
     public static String longestCommonPrefix(String[] strs) {
-                String longer = "";
+        String longer = "";
         String temp = strs[0];
         String temp2;
         boolean matched = false;
         for (int j = 0; j < temp.length(); j++) {
             temp2 = temp.substring(0,temp.length() - j);
-            System.out.println("TEMP2:" + temp2);
+//            System.out.println("TEMP2: "+temp2);
             matched = true;
             for (int i = 1; i < strs.length; i++) {
-                System.out.println("Index of-->"+ strs[i].indexOf(temp2));
-                if ( (strs[i].indexOf(temp2)==-1)) {
+//                if ( (strs[i].indexOf(temp2)==-1)) {
+                if ( (!strs[i].startsWith(temp2))) {
                     matched = false;
                     break;
                 }
@@ -56,16 +61,9 @@ public class LongestCommonPrefix {
             }
 
         }
-        System.out.println("LONGER" + longer);
+//        System.out.println("LONGER: " + longer);
         return longer;
     }
-    private static String reverseString(String str){
-        StringBuilder nstr = new StringBuilder();
-        for (int i=0; i<str.length(); i++)
-        {
-            char ch= str.charAt(i); //extracts each character
-            nstr.insert(0, ch); //adds each character in front of the existing string
-        }
-        return nstr.toString();
-    }
+
+
 }
